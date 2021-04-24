376c7d822537c59fd2aba159edd25d204c307552
wvhovey <wvhovey@gmail.com>
2021-04-24 18:00:25 -0400

    Course Portal Home Page

--- js/date.js
+++ js/date.js
@@ -1,2 +1,5 @@
-var dt = new Date(document.lastModified);
-document.getElementById('date-time').innerHTML=dt;
+var options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US', options);
+
+var options = {year: 'numeric'};
+document.querySelector('#current-year').textContent = new Date().toLocaleDateString('en-US', options);
