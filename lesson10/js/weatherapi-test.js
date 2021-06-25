3d5477172493d594138f4e0c5d7bb4dfe09c4578
wvhovey <wvhovey@gmail.com>
2021-06-25 14:38:10 -0400

    W10 Preston Draft

---
+++ lesson10/js/weatherapi-test.js
@@ -0,0 +1,15 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=90899f2531f315e25562f0f4572b08c9";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
