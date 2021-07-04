122f9c5967a22d7d59f25ebc83700028174d457c
wvhovey <wvhovey@gmail.com>
2021-07-03 20:37:02 -0400

    Final Weather Site 2nd Draft

---
+++ lesson11/js/town-data-preston.js
@@ -0,0 +1,38 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++ ) {
+        
+        if (i == 6) {
+            let card = document.createElement('section');
+            let h3 = document.createElement('h3');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let p3 = document.createElement('p');
+
+            card.className = "town-event-list" + i;
+            h3.textContent = "Yearly Events";
+            
+            p1.textContent = towns[i].events[0];
+            p2.textContent = towns[i].events[1];
+            p3.textContent = towns[i].events[2];
+
+            card.appendChild(h3);
+            card.appendChild(p1);
+            card.appendChild(p2);
+            card.appendChild(p3);
+    
+            document.querySelector('div.event-list').appendChild(card);
+
+        }
+    }
+
+
+
+  });
